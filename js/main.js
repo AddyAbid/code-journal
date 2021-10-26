41626fa9b136e6a5e3447ba62ccb3c302474cb69
Addy Abid <addy.abeed@gmail.com>
2021-10-25 17:28:02 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -30,3 +30,52 @@ function formSubmit(event) {
   $entryForm.reset();
   renderImage();
 }
+
+/* <ul class="row">
+    <li class="column-full">
+      <div class="row">
+        <div class="column-half">
+          <img class="width-100" src="https://res.cloudinary.com/teepublic/image/private/s--17I0z7Hp--/c_scale,h_704/c_lpad,g_north_west,h_801,w_1802,x_174,y_48/c_crop,h_801,w_691,x_125/c_mfit,g_north_west,u_misc:Mug%20Effect%20Coffee3%20Left/e_displace,fl_layer_apply,x_14,y_-2/c_mfit,g_north_east,u_misc:Mug%20Effect%20Coffee3%20Right/e_displace,fl_layer_apply,x_-14,y_-2/c_crop,h_801,w_656/g_north_west,l_upload:v1466696262:production:blanks:w00xdkhjelyrnp8i8wxr,x_-410,y_-235/b_rgb:000000/c_limit,f_auto,h_630,q_90,w_630/v1539381322/production/designs/3309111_0.jpg">
+        </div>
+        <div class="column-half">
+          <h2 class="f-gray font-weight">Javascript Mug</h2>
+          <p class="font-family">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum neque sapiente, nesciunt quaerat voluptatibus vitae sed provident ad odit error mollitia? Nemo at dolore expedita optio corporis culpa inventore quisquam?</p>
+        </div>
+      </div>
+    </li>
+  </ul> */
+
+function renderEntry(entry) {
+
+  var $li = document.createElement('li');
+  $li.setAttribute('class', 'column-full');
+  $ul.appendChild($li);
+
+  var $divRow = document.createElement('div');
+  $divRow.setAttribute('class', 'row');
+  $li.appendChild($divRow);
+
+  var $divColHalf = document.createElement('div');
+  document.setAttribute('class', 'column-half');
+  $divRow.appendChild($divColHalf);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('class', 'width-100');
+  $img.setAttribute('src', entry.src);
+  $divColHalf.appendChild($img);
+
+  var $divColHalf2 = document.createElement('div');
+  $divColHalf2.setAttribute('class', 'column-half');
+  $divRow.appendChild($divColHalf2);
+
+  var $h2 = document.createElement('h2');
+  $h2.setAttribute('class', 'f-gray font-weight');
+  $divColHalf2.appendChild($h2);
+
+  var $description = document.createElement('p');
+  $description.setAttribute('class', 'font-family');
+  $divColHalf2.appendChild($description);
+
+}
+var $ul = document.createElement('ul');
+$ul.setAttribute('class', 'row');
